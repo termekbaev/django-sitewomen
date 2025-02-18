eb84c223e25487d347f84fe145ec6f59cb821a1c
Timur <t-ermekbaev@mail.ru>
2025-02-19 01:15:25 +0300

    Added search fields and filters(rightside)

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin, messages
 from .models import Women, Category
 
 
+class MarriedFilter(admin.SimpleListFilter):
+    title = 'Статус женщин'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('married', 'Замужем'),
+            ('single', 'Не замужем'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+        elif self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
+
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
     list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info', )
@@ -10,6 +26,8 @@ class WomenAdmin(admin.ModelAdmin):
     list_editable = ('is_published',)
     list_per_page = 5
     actions = ['set_published', 'set_draft']
+    search_fields = ['title__startswith', 'cat__name']
+    list_filter = [MarriedFilter, 'cat__name', 'is_published']
 
     @admin.display(description='Краткое описание', ordering='content')
     def brief_info(self, women: Women):
