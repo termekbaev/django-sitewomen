d36c3f1f79a7a92eb9d6bfe4d2631585d53935d7
Timur <t-ermekbaev@mail.ru>
2025-02-19 02:44:05 +0300

    Setting up the Record editing form

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -20,6 +20,11 @@ class MarriedFilter(admin.SimpleListFilter):
 
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
+    fields = ['title', 'slug', 'content', 'cat', 'husband', 'tags']
+    # exclude = ['tags', 'is_published']
+    # readonly_fields = ['slug']
+    prepopulated_fields = {'slug': ('title', )}
+    # filter_horizontal = ['tags']
     list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info', )
     list_display_links = ('title', )
     ordering = ['time_create', 'title']
