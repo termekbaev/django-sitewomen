75f0930771df6d95137fe84e9d9721c7feb6c879
Timur <t-ermekbaev@mail.ru>
2025-02-20 15:19:11 +0300

    Added form class "AddPostForm"

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 
+from .forms import AddPostForm
 from women.models import Women, Category, TagPost
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
@@ -41,7 +42,19 @@ def show_post(request, post_slug):
 
 
 def addpage(request):
-    return render(request, 'women/addpage.html', {'menu': menu, 'title': 'Добавление статьи'})
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = AddPostForm()
+
+    data = {
+        'menu': menu,
+        'title': 'Добавление статьи',
+        'form': form,
+    }
+    return render(request, 'women/addpage.html', data)
 
 
 def contact(request):
