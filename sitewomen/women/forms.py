a9aadb4e74de296a4eb0f09ea2e1fc26ca631df5
Timur <t-ermekbaev@mail.ru>
2025-02-23 21:35:48 +0300

    Модифицировали. Заменил наследование от forms.Form на forms.ModelForm, что упростило код

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from django.core.validators import MinLengthValidator, MaxLengthValidator, BaseValidator
 from django.db.models import Manager
+from django.template.defaultfilters import title
 from django.utils.deconstruct import deconstructible
 from django.core.exceptions import ValidationError
 
-from .models import Category, Husband
+from .models import Category, Husband, Women
 
 
 @deconstructible
@@ -20,27 +21,23 @@ class RussianValidator:
             raise ValidationError(self.message, code=self.code)
 
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, min_length=5, label='Заголовок',
-                            widget=forms.TextInput(attrs={'class': 'form-input'}),
-                            error_messages={
-                                'min_length': 'Слишком короткий заголовок',
-                                'required': 'Без заголовка никак',
-                            })
-    slug = forms.SlugField(max_length=255, label='URL', validators=[
-                                                            MinLengthValidator(5, message='Максимум 5 символов'),
-                                                            MaxLengthValidator(100, message='Максимум 100 символов'),
-                                                        ])
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label='Контент')
-    is_published = forms.BooleanField(required=False, initial=True, label='Статус')
+class AddPostForm(forms.ModelForm):
+
     cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label='Не выбрано', label='Категории')
     husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False, empty_label='Не замужем', label='Муж')
 
+    class Meta:
+        model = Women
+        fields = ['title', 'slug', 'content', 'is_published', 'cat', 'husband', 'tags']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 50, 'rows': 5}),
+        }
+        labels = {'slug': 'URL'}
+
     def clean_title(self):
         title = self.cleaned_data['title']
-        ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщбыъэюя0123456789- "
-
-        if not(set(title) <= set(ALLOWED_CHARS)):
-            raise ValidationError("Должны присутствовать только русские символы, дефис или пробел.")
+        if len(title) > 50:
+            raise ValidationError('Длина превышает 50 символов')
 
         return title
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -45,12 +45,14 @@ def addpage(request):
     if request.method == 'POST':
         form = AddPostForm(request.POST)
         if form.is_valid():
-            #print(form.cleaned_data)
-            try:
-                Women.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Ошибка добавления поста')
+            # print(form.cleaned_data)
+            # try:
+            #     Women.objects.create(**form.cleaned_data)
+            #     return redirect('home')
+            # except:
+            #     form.add_error(None, 'Ошибка добавления поста')
+            form.save()
+            return redirect('home')
     else:
         form = AddPostForm()
 
