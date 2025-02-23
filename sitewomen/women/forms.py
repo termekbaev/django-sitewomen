a7399caf6f4c2edde0c1136e1f981d1bab63913c
Timur <t-ermekbaev@mail.ru>
2025-02-23 22:56:24 +0300

    Прописал загрузку файлов на сервер. В "о сайте"

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -41,3 +41,7 @@ class AddPostForm(forms.ModelForm):
             raise ValidationError('Длина превышает 50 символов')
 
         return title
+
+
+class UploadFileForm(forms.Form):
+    file = forms.ImageField(label='Файл')
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
 
-from .forms import AddPostForm
+from .forms import AddPostForm, UploadFileForm
 from women.models import Women, Category, TagPost
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
@@ -25,8 +25,21 @@ def index(request):
     return render(request, 'women/index.html', context=data)
 
 
+def handle_uploaded_file(f):
+    with open(f'uploads/{f.name}', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+
 def about(request):
-    return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(form.cleaned_data['file'])
+    else:
+        form = UploadFileForm()
+    return render(request, 'women/about.html',
+                  {'title': 'О сайте', 'menu': menu, 'form': form})
 
 
 def show_post(request, post_slug):
